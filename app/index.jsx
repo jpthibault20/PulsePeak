9779602c93926e8492892553c44f176b68d76e80
jpthibault20 <tjeanpierre757@gmail.com>
2024-06-26 15:07:06 +0200

    "Update App.js to display 'PulsePeak coming soon' text instead of default message"

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text className="font-semibold text-xl">PulsePeak comming soon</Text>
             <StatusBar style="auto" />
         </View>
     );
