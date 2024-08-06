d7647d986e870c899ac9113ce59bf074d3b1f54e
thibault <tjeanpierre757@gmail.com>
2024-08-06 09:00:25 +0200

    feat: calendar / home page (monthly view) front

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,20 +5,23 @@ import { generateCalendar } from '../../lib/calendar'
 import { SwitchView } from '../../components/tabs/Calendar/switch'
 import { MonthlyView } from '../../components/tabs/Calendar/MonthlyView'
 import { WeeklyView } from '../../components/tabs/Calendar/WeeklyView'
+import { DetailDay } from '../../components/tabs/Calendar/DetailDay'
 
 const Home = () => {
     const [calendar, setCalendar] = useState([]);
     const [date, setDate] = useState(new Date());
     const [switchValue, setSwitchValue] = useState("M");
+    const [selectedDay, setSelectedDay] = useState(new Date(new Date().getTime() + 7200000));
 
     useEffect(() => {
         setCalendar(generateCalendar(date.getFullYear(), date.getMonth()));
-    }, [])
+    }, []);
+
 
     return (
         <View className="bg-white h-full">
             <TemplatePP isLoading={true} />
-            <View className="h-3/5 w-full bg-[#F4F4F4F4] bor">
+            <View className="h-[55%] w-full bg-[#F4F4F4F4]">
                 <View className="flex flex-row justify-between items-center p-6">
                     <View>
                         <Text className="font-mregular text-xl">
@@ -34,15 +37,29 @@ const Home = () => {
                     </View>
                 </View>
 
-                <View className="w-full border h-full">
-                    {switchValue === "M" ? <MonthlyView calendar={calendar} /> : <WeeklyView calendar={calendar} />}
+                <View className="w-full">
+                    {switchValue === "M" ?
+                        <MonthlyView
+                            calendar={calendar}
+                            selectedDay={selectedDay}
+                            setSelectedDay={setSelectedDay}
+                        />
+                        :
+                        <WeeklyView calendar={calendar}
+                        />}
+                </View>
+
+                <View className="items-center mt-8">
+                    <View className="border rounded-xl w-[65px] bg-[#0552B1] border-[#0552B1]">
+                    </View>
                 </View>
 
             </View>
             <View className="h-full w-full">
-                <Text>
-                    Detail day calendar
-                </Text>
+                <DetailDay
+                    selectedDay={selectedDay}
+                    calendar={calendar}
+                />
             </View>
         </View>
     )
--- components/tabs/Calendar/MonthlyView.jsx
+++ components/tabs/Calendar/MonthlyView.jsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { splitPerDay } from '../../../lib/calendar'
+import { Day } from './Day'
 
-const days = ['LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM']
 
-const TitleColumn = ({ title }) => {
+
+
+
+export const MonthlyView = ({ calendar, selectedDay, setSelectedDay }) => {
+    const { nameOfDay, daysGroupedByWeekday } = splitPerDay(calendar);
+
     return (
-        <Text className="font-mmedium text-md mx-3">
-            {title}
-        </Text>
-    )
-}
-
-export const MonthlyView = ({ calendar }) => {
-    return (
-        <View className="w-full items-center">
-            <View className="flex flex-row ">
-                {days.map((day, index) => (
-                    <TitleColumn key={index} title={day} />
-                ))}
-            </View>
-
-            <View>
-                <Text className="font-mbold text-xl">
-                    monthly view
-                </Text>
-            </View>
-        </View >
+        <View className=" flex-row w-full justify-between px-2">
+            {nameOfDay.map((dayName, index) => (
+                <View key={index} className="items-center">
+                    <Text className="font-mregular text-lg py-3">
+                        {dayName}
+                    </Text>
+                    {daysGroupedByWeekday[dayName].map((event, dayIndex) => (
+                        <View key={dayIndex}>
+                            <Day
+                                event={event}
+                                setSelectedDay={setSelectedDay}
+                                touchableDay={selectedDay.getFullYear() === event.year && selectedDay.getMonth() + 1 === event.month && selectedDay.getDate() === event.day ? true : false}
+                            />
+                        </View>
+                    ))}
+                </View>
+            ))}
+        </View>
     )
 }
